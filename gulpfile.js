63603aae626438aa6c282295483a5072a7178512
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2020-02-18 09:25:37 +0200

    Переделка для урока

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ require('../../gulp/gulp-init.js')({HTML: '.'});
 
 const comb = require('../../gulp/tasks/comb');
 const cs = require('../../gulp/tasks/create-structure');
-const scss = require('../../gulp/tasks/scss.js').scss;
+const scss = require('../../gulp/tasks/scss.js').scssOnly;
 const mincss = require('../../gulp/tasks/mincss');
 const uglifyes = require('../../gulp/tasks/uglify').uglifyes;
 const { sync, syncInit } = require('../../gulp/tasks/sync');
 
 function watchFiles () {
     syncInit();
-    watch($.path.scss.files, series(scss, mincss));
-    watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
-    watch($.path.html.files, sync);
+    watch($.PATH.scss.files, series(scss, mincss));
+    watch([$.PATH.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
+    watch($.PATH.html.files, sync);
 }
 
 task('cs', cs);
